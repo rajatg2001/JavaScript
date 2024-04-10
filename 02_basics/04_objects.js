b9a74fc95dd08b9ad959694ca47ca2a2bb85d95a
RajatGupta <132397837+rajatg2001@users.noreply.github.com>
2024-04-10 21:08:20 +0000

    ddone

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -52,3 +52,28 @@ console.log(Object.keys(tinderUser)); // return an array
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
